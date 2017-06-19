37eaf904a8cc1c5c4699f96572a80d4c41a0e5ae
drobazko <drobazko@gmail.com>
2017-06-19 20:59:31 +0300

    Global replacement tab with double space

--- lib/combiner.rb
+++ lib/combiner.rb
@@ -5,41 +5,41 @@
 # output:
 # - enumerator for the combined elements
 class Combiner
-	def initialize(&key_extractor)
-		@key_extractor = key_extractor
-	end
+  def initialize(&key_extractor)
+    @key_extractor = key_extractor
+  end
 
-	def key(value)
-		value && @key_extractor.call(value)
-	end
+  def key(value)
+    value && @key_extractor.call(value)
+  end
 
-	def combine(*enumerators)
-		Enumerator.new do |yielder|
-			last_values = Array.new(enumerators.size)
-			while not enumerators.all?(&:nil?)
-				last_values.each_with_index do |value, index|
-					if value.nil? and enumerators[index]
-						begin
-							last_values[index] = enumerators[index].next
-						rescue StopIteration
-							enumerators[index] = nil
-						end
-					end
-				end
+  def combine(*enumerators)
+    Enumerator.new do |yielder|
+      last_values = Array.new(enumerators.size)
+      while not enumerators.all?(&:nil?)
+        last_values.each_with_index do |value, index|
+          if value.nil? and enumerators[index]
+            begin
+              last_values[index] = enumerators[index].next
+            rescue StopIteration
+              enumerators[index] = nil
+            end
+          end
+        end
 
-				done = enumerators.all?(&:nil?) and last_values.compact.empty?
-				unless done
-					min_key = last_values.map { |e| key(e) }.compact.min
-					values = Array.new(last_values.size)
-					last_values.each_with_index do |value, index|
-						if key(value) == min_key
-							values[index] = value
-							last_values[index] = nil
-						end
-					end
-					yielder.yield(values)
-				end
-			end
-		end
-	end
+        done = enumerators.all?(&:nil?) and last_values.compact.empty?
+        unless done
+          min_key = last_values.map { |e| key(e) }.compact.min
+          values = Array.new(last_values.size)
+          last_values.each_with_index do |value, index|
+            if key(value) == min_key
+              values[index] = value
+              last_values[index] = nil
+            end
+          end
+          yielder.yield(values)
+        end
+      end
+    end
+  end
 end
--- modifier.rb
+++ modifier.rb
@@ -4,193 +4,193 @@ require 'date'
 
 def latest
   Dir["#{Dir.pwd}/data/project_*_*_performancedata.txt"]
-		.map{|v| v =~ /(\d+-\d+-\d+)\_\D/; { fname: v, date: DateTime.parse($1) } }
-		.sort_by{|v| v[:date]}
-		.last[:fname]
+    .map{|v| v =~ /(\d+-\d+-\d+)\_\D/; { fname: v, date: DateTime.parse($1) } }
+    .sort_by{|v| v[:date]}
+    .last[:fname]
 end
 
 class String
-	def from_german_to_f
-		self.gsub(',', '.').to_f
-	end
+  def from_german_to_f
+    self.gsub(',', '.').to_f
+  end
 end
 
 class Float
-	def to_german_s
-		self.to_s.gsub('.', ',')
-	end
+  def to_german_s
+    self.to_s.gsub('.', ',')
+  end
 end
 
 class Modifier
-	KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
+  KEYWORD_UNIQUE_ID = 'Keyword Unique ID'
 
-	LAST_VALUE_WINS = [
-		'Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 
-		'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD'
-	]
+  LAST_VALUE_WINS = [
+    'Account ID', 'Account Name', 'Campaign', 'Ad Group', 'Keyword', 'Keyword Type', 'Subid', 'Paused', 'Max CPC', 
+    'Keyword Unique ID', 'ACCOUNT', 'CAMPAIGN', 'BRAND', 'BRAND+CATEGORY', 'ADGROUP', 'KEYWORD'
+  ]
 
-	LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
+  LAST_REAL_VALUE_WINS = ['Last Avg CPC', 'Last Avg Pos']
 
-	INT_VALUES = [
-		'Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 
-		'ADGROUP - Clicks', 'KEYWORD - Clicks'
-	]
+  INT_VALUES = [
+    'Clicks', 'Impressions', 'ACCOUNT - Clicks', 'CAMPAIGN - Clicks', 'BRAND - Clicks', 'BRAND+CATEGORY - Clicks', 
+    'ADGROUP - Clicks', 'KEYWORD - Clicks'
+  ]
 
-	FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
+  FLOAT_VALUES = ['Avg CPC', 'CTR', 'Est EPC', 'newBid', 'Costs', 'Avg Pos']
 
-	COMISSION_NUMBERS = ['number of commissions']
+  COMISSION_NUMBERS = ['number of commissions']
 
-	COMISSION_VALUES = [
-		'Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 
-		'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value'
-	]
+  COMISSION_VALUES = [
+    'Commission Value', 'ACCOUNT - Commission Value', 'CAMPAIGN - Commission Value', 'BRAND - Commission Value', 
+    'BRAND+CATEGORY - Commission Value', 'ADGROUP - Commission Value', 'KEYWORD - Commission Value'
+  ]
 
-	DEFAULT_CSV_OPTIONS = { col_sep: '|', headers: :first_row }
-	EXTENDED_CSV_OPTIONS = DEFAULT_CSV_OPTIONS.merge({ row_sep: "\r\n" })
+  DEFAULT_CSV_OPTIONS = { col_sep: '|', headers: :first_row }
+  EXTENDED_CSV_OPTIONS = DEFAULT_CSV_OPTIONS.merge({ row_sep: "\r\n" })
 
   LINES_PER_FILE = 120000
 
-	def initialize(saleamount_factor:, cancellation_factor:)
-		@saleamount_factor = saleamount_factor
-		@cancellation_factor = cancellation_factor
-	end
+  def initialize(saleamount_factor:, cancellation_factor:)
+    @saleamount_factor = saleamount_factor
+    @cancellation_factor = cancellation_factor
+  end
 
-	def modify(output, input)
-		input = sort(input)
+  def modify(output, input)
+    input = sort(input)
 
-		input_enumerator = lazy_read(input)
+    input_enumerator = lazy_read(input)
 
-		combiner = Combiner.new do |value|
-			value[KEYWORD_UNIQUE_ID]
-		end.combine(input_enumerator)
+    combiner = Combiner.new do |value|
+      value[KEYWORD_UNIQUE_ID]
+    end.combine(input_enumerator)
 
-		merger = Enumerator.new do |yielder|
-			while true
-				begin
-					list_of_rows = combiner.next
-					merged = combine_hashes(list_of_rows)
-					yielder.yield(combine_values(merged))
-				rescue StopIteration
-					break
-				end
-			end
-		end
+    merger = Enumerator.new do |yielder|
+      while true
+        begin
+          list_of_rows = combiner.next
+          merged = combine_hashes(list_of_rows)
+          yielder.yield(combine_values(merged))
+        rescue StopIteration
+          break
+        end
+      end
+    end
 
     done = false
     file_index = 0
     file_name = output.gsub('.txt', '')
     while not done do
-		  CSV.open(file_name + "_#{file_index}.txt", 'wb', EXTENDED_CSV_OPTIONS) do |csv|
-			  headers_written = false
+      CSV.open(file_name + "_#{file_index}.txt", 'wb', EXTENDED_CSV_OPTIONS) do |csv|
+        headers_written = false
         line_count = 0
-			  while line_count < LINES_PER_FILE
-				  begin
-					  merged = merger.next
-					  if not headers_written
-						  csv << merged.keys
-						  headers_written = true
+        while line_count < LINES_PER_FILE
+          begin
+            merged = merger.next
+            if not headers_written
+              csv << merged.keys
+              headers_written = true
               line_count +=1
-					  end
-					  csv << merged
+            end
+            csv << merged
             line_count +=1
-				  rescue StopIteration
+          rescue StopIteration
             done = true
-					  break
-				  end
-			  end
+            break
+          end
+        end
         file_index += 1
-		  end
+      end
     end
-	end
+  end
 
-	def sort(file)
-		output = "#{file}.sorted"
-		content_as_table = parse(file)
+  def sort(file)
+    output = "#{file}.sorted"
+    content_as_table = parse(file)
 
-		headers = content_as_table.headers
-		index_of_key = headers.index('Clicks')
-		content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
-		write(content, headers, output)
-		return output
-	end
+    headers = content_as_table.headers
+    index_of_key = headers.index('Clicks')
+    content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
+    write(content, headers, output)
+    return output
+  end
 
-	private
+  private
 
-	def combine(merged)
-		result = []
-		merged.each do |_, hash|
-			result << combine_values(hash)
-		end
-		result
-	end
+  def combine(merged)
+    result = []
+    merged.each do |_, hash|
+      result << combine_values(hash)
+    end
+    result
+  end
 
-	def combine_values(hash)
-		LAST_VALUE_WINS.each do |key|
-			next unless hash[key]
-			hash[key] = hash[key].last
-		end
-		LAST_REAL_VALUE_WINS.each do |key|
-			next unless hash[key]
-			hash[key] = hash[key].select {|v| not (v.nil? or v == 0 or v == '0' or v == '')}.last
-		end
-		INT_VALUES.each do |key|
-			next unless (hash[key] && hash[key][0])
-			hash[key] = hash[key][0].to_s
-		end
-		FLOAT_VALUES.each do |key|
-			next unless (hash[key] && hash[key][0])
-			hash[key] = hash[key][0].from_german_to_f.to_german_s
-		end
-		COMISSION_NUMBERS.each do |key|
-			next unless (hash[key] && hash[key][0])
-			hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
-		end
-		COMISSION_VALUES.each do |key|
-			next unless (hash[key] && hash[key][0])
-			hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
-		end
-		hash
-	end
+  def combine_values(hash)
+    LAST_VALUE_WINS.each do |key|
+      next unless hash[key]
+      hash[key] = hash[key].last
+    end
+    LAST_REAL_VALUE_WINS.each do |key|
+      next unless hash[key]
+      hash[key] = hash[key].select {|v| not (v.nil? or v == 0 or v == '0' or v == '')}.last
+    end
+    INT_VALUES.each do |key|
+      next unless (hash[key] && hash[key][0])
+      hash[key] = hash[key][0].to_s
+    end
+    FLOAT_VALUES.each do |key|
+      next unless (hash[key] && hash[key][0])
+      hash[key] = hash[key][0].from_german_to_f.to_german_s
+    end
+    COMISSION_NUMBERS.each do |key|
+      next unless (hash[key] && hash[key][0])
+      hash[key] = (@cancellation_factor * hash[key][0].from_german_to_f).to_german_s
+    end
+    COMISSION_VALUES.each do |key|
+      next unless (hash[key] && hash[key][0])
+      hash[key] = (@cancellation_factor * @saleamount_factor * hash[key][0].from_german_to_f).to_german_s
+    end
+    hash
+  end
 
-	def combine_hashes(list_of_rows)
-		keys = []
-		list_of_rows.each do |row|
-			next if row.nil?
-			row.headers.each do |key|
-				keys << key
-			end
-		end
-		result = {}
-		keys.each do |key|
-			result[key] = []
-			list_of_rows.each do |row|
-				result[key] << (row.nil? ? nil : row[key])
-			end
-		end
-		result
-	end
+  def combine_hashes(list_of_rows)
+    keys = []
+    list_of_rows.each do |row|
+      next if row.nil?
+      row.headers.each do |key|
+        keys << key
+      end
+    end
+    result = {}
+    keys.each do |key|
+      result[key] = []
+      list_of_rows.each do |row|
+        result[key] << (row.nil? ? nil : row[key])
+      end
+    end
+    result
+  end
 
-	def parse(file)
-		p file
-		CSV.read(file, DEFAULT_CSV_OPTIONS)
-	end
+  def parse(file)
+    p file
+    CSV.read(file, DEFAULT_CSV_OPTIONS)
+  end
 
-	def lazy_read(file)
-		Enumerator.new do |yielder|
-			CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
-				yielder.yield(row)
-			end
-		end
-	end
+  def lazy_read(file)
+    Enumerator.new do |yielder|
+      CSV.foreach(file, DEFAULT_CSV_OPTIONS) do |row|
+        yielder.yield(row)
+      end
+    end
+  end
 
-	def write(content, headers, output)
-		CSV.open(output, 'wb', EXTENDED_CSV_OPTIONS) do |csv|
-			csv << headers
-			content.each do |row|
-				csv << row
-			end
-		end
-	end
+  def write(content, headers, output)
+    CSV.open(output, 'wb', EXTENDED_CSV_OPTIONS) do |csv|
+      csv << headers
+      content.each do |row|
+        csv << row
+      end
+    end
+  end
 end
 
 modified = input = latest
